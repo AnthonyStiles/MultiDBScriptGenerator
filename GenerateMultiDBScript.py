dd17754e68a11c46f3cf92dd65cfbd630235dee7
Anthony Stiles <stilant456@gmail.com>
2024-08-10 21:07:04 +0100

    initial commit

---
+++ GenerateMultiDBScript.py
@@ -0,0 +1,29 @@
+import os
+import yaml
+
+print('GETTING CONFIG FILE...')
+config_file_name = 'config.yaml'
+
+with open(config_file_name, 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+databases = config['databases']
+scripts = config['scripts']
+
+print(f'FOUND {len(databases)} database(s) AND {len(scripts)} script(s)')
+
+print('GENERATING SCRIPT...')
+
+script_name = 'output.sql'
+
+with open (script_name, 'w') as output:
+    for db in databases:
+        output.write(f'USE {db}\n\n')
+        for script in scripts:
+            output.write(f':r {script}')
+        output.write('\n\n\n')
+
+    output.write("--Now just open this in SQL Server Management Studio, Click the Query tab and select SQLCMD Mode (its near the bottom) and send it!\n")
+    output.write("--You're welcome...")
+
+print('DONE!')
